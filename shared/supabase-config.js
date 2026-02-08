1909472c857720ef8bab9cf72303dabfe509c905
Inspecting <96089121+Inspecting@users.noreply.github.com>
2026-02-07 22:42:00 -0600

    Delete shared directory

--- shared/supabase-config.js
+++
@@ -1,4 +0,0 @@
-window.BILM_SUPABASE_CONFIG = {
-  url: 'https://veoydbjuggetxwfgryzc.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZlb3lkYmp1Z2dldHh3ZmdyeXpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA1MTI2MTAsImV4cCI6MjA4NjA4ODYxMH0.6VHciP2HKD1w4Le9df8xV77EvHzsj1fXJceYDbNGQnU'
-};
