fda455a74b0a5e56f5a304760990129ff571e691
Inspecting <96089121+Inspecting@users.noreply.github.com>
2026-02-16 13:01:30 -0600

    Update viewer.js

--- movies/viewer.js
+++ movies/viewer.js
@@ -127,7 +127,7 @@ function buildMovieUrl(server) {
   if (!contentId) return '';
   switch (server) {
     case 'vidsrc':
-      return `https://vsrc.su/embed/movie/${contentId}`;
+      return `https://vidsrc-embed.ru/embed/tv/${tmdbId || imdbId}`;
     case 'godrive':
       return imdbId ? `https://godriveplayer.com/player.php?imdb=${imdbId}` : '';
     case 'multiembed':
