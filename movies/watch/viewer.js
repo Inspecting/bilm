ae18ad749463bba20262ade971c6312af2380073
Inspecting <96089121+Inspecting@users.noreply.github.com>
2026-02-22 20:58:09 -0600

    Remove iframe sandbox helper usage in movie and TV viewers

--- movies/watch/viewer.js
+++ movies/watch/viewer.js
@@ -167,31 +167,22 @@ function buildReloadableUrl(url) {
 
 function refreshIframe(url) {
   const token = ++iframeRefreshToken;
-  if (window.BilmEmbedSandbox?.setSandboxedIframeSrc) {
-    window.BilmEmbedSandbox.setSandboxedIframeSrc(iframe, 'about:blank');
-  } else {
-    iframe.src = 'about:blank';
-  }
+  iframe.removeAttribute('sandbox');
+  iframe.src = 'about:blank';
 
   window.setTimeout(() => {
     if (token !== iframeRefreshToken) return;
     const reloadUrl = buildReloadableUrl(url);
-    if (window.BilmEmbedSandbox?.setSandboxedIframeSrc) {
-      window.BilmEmbedSandbox.setSandboxedIframeSrc(iframe, reloadUrl);
-    } else {
-      iframe.src = reloadUrl;
-    }
+    iframe.removeAttribute('sandbox');
+    iframe.src = reloadUrl;
   }, 60);
 }
 
 function updateIframe() {
   if (!contentId) {
     console.warn('No id parameter provided.');
-    if (window.BilmEmbedSandbox?.setSandboxedIframeSrc) {
-      window.BilmEmbedSandbox.setSandboxedIframeSrc(iframe, '');
-    } else {
-      iframe.src = '';
-    }
+    iframe.removeAttribute('sandbox');
+    iframe.src = '';
     return;
   }
   let url = buildMovieUrl(currentServer);
